d8b9361fdbd888361df0c0146766fda98ceb3a4d
Bruno974 <hoarau.bruno@gmail.com>
2021-10-08 01:17:19 +0200

    update css

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 const header = document.getElementsByTagName('header')[0];
 const articleRecipes = document.getElementById('article_recipes');
 const inputIngredients = document.getElementById('ingredientsInput');
-// let suggests;
+const inputUstensils = document.getElementById('ustensils');
+const inputAppliance = document.getElementById('appliance');
 const inputSearch = document.getElementsByTagName('input');
 const tags = document.getElementById('tags');
 let suggests;
 
 // any elmts
 let ingredientsList = [];
-let appareilsList = [];
+let applianceList = [];
 let ustensilsList = [];
 let tagsArray = [];
 let tagsIngredients = [];
@@ -65,7 +66,7 @@ function updateData(){
   console.log(tagsArray)
   tagsArray.forEach ( elmt => {
     articleRecipes.innerHTML = "";
-    if (ingredientsList.includes(elmt) || ustensilsList.includes(elmt) || appareilsList.includes(elmt)){
+    if (ingredientsList.includes(elmt) || ustensilsList.includes(elmt) || applianceList.includes(elmt)){
       var cache = {};
       updateRecipe = updateRecipe.filter(function(elem){
         return cache[elem.id]?0:cache[elem.id]=1;
@@ -93,12 +94,12 @@ function suggestList(array){
           // app = app[0].toUpperCase() + app.slice(1);
           ustensilsList.push(app)
         });
-      appareilsList.push(elmt.appliance);
+      applianceList.push(elmt.appliance);
     }
   );
   ingredientsList = [...new Set(ingredientsList)];
   ustensilsList = [...new Set(ustensilsList)];
-  appareilsList = [...new Set(appareilsList)];
+  applianceList = [...new Set(applianceList)];
 }
 // factory node
 const elmtFactory = (nodeName, attribute, ...children) => {
@@ -126,9 +127,7 @@ const title = elmtFactory('h1', { id: 'header_h1'}, "Les petits plats")
 const formSearchBar = elmtFactory('form', { class: "form_search"},elmtFactory('input', { type: 'text', class: 'header_nav', autocomplete: 'off', placeholder: "Rechercher un ingredient, appareil, ustensiles ou une recette"},),
 elmtFactory('button', { type: 'submit'},
 elmtFactory('i',{ class: 'bi bi-search'}, ) ),
-    // elmtFactory('i',{ class: 'bi bi-search'}, ),
-    elmtFactory('div', { id: 'suggestsGlobal'},
-    )  )
+    )
 const topNav = elmtFactory('input', { type: 'text', class: 'header_nav'},
   elmtFactory('i',{ class: 'bi bi-search'}, ));
 
@@ -244,13 +243,12 @@ function showSuggest(array, e, input){
                     updateRecipe.push(elmt);
                   }
                 }
-              } else if (key === 'name' && value.toLowerCase().includes(inputValue.toLowerCase())) {
               }
             }
       }
     }
   })
-  suggests.innerHTML = "";
+  // suggests.innerHTML = "";
   itemArray.forEach( sug => {
     ingrdSuggest = elmtFactory('p', { class: "suggest", value: inputId, onclick: "suggestFunction(this)"}, sug )
     suggests.appendChild(ingrdSuggest);
@@ -331,4 +329,126 @@ updateRecipe.forEach( elmt => {
   console.log(updateRecipe);
   element.textContent = "vide";
   render();
+}
+
+const spans = document.getElementsByTagName('span');
+
+for ( let span of spans){
+  span.addEventListener('click', (e) => {
+    // e.preventDefault();
+    let spanValue = span.getAttribute('value');
+    let spanValueM = spanValue[0].toUpperCase() + spanValue.slice(1);
+    let suggestById = "suggests"+ spanValueM ;
+    console.log(suggestById);
+    let suggestId = document.getElementById(suggestById);
+    suggestId.innerHTML = "";
+    // let inputId = document.getElementById(suggestById);
+    let nomming = spanValue+"List";
+    let dynamicName = eval(nomming)
+    for( let li of dynamicName){
+      let listSuggest = elmtFactory('p', { class: "suggest", value: spanValue, onclick: "listFunction(this)"}, li );
+      suggestId.appendChild(listSuggest);
+    }
+  })
+}
+
+
+function listFunction(element){
+  let cacheUpdate = [];
+  let tag = [];
+  tags.innerHTML = "";
+  let text = element.textContent;
+  let valuElmt = element.getAttribute('value');
+  console.log(valuElmt)
+  let valueM = valuElmt[0].toUpperCase() + valuElmt.slice(1);
+    let suggestId = "suggests"+ valueM ;
+    console.log(eval(suggestId));
+  
+
+  if( valuElmt === 'ingredients' ){
+    tagsIngredients.push(text);
+    tagsIngredients = [...new Set(tagsIngredients)];
+    console.log(tagsIngredients.length);
+  } else if( valuElmt === 'appliance'){
+    tagsAplliance.push(text);
+    tagsAplliance = [...new Set(tagsAplliance)];
+    console.log(tagsAplliance);
+  } else {
+    tagsUstensils.push(text)
+    tagsUstensils = [...new Set(tagsUstensils)];
+    console.log(tagsUstensils);
+  }
+
+  tagsArray.push(text);
+tagsArray = [...new Set(tagsArray)];
+tagsArray.forEach( elmt => {
+  tag = elmtFactory('p', { class: "bg-primary text-white"}, elmt);
+  tags.appendChild(tag);
+})
+
+  if( updateRecipe.length === 0){
+    sortArray( recipesArray, cacheUpdate, valuElmt, text );
+    eval(suggestId).innerHTML = "";
+  } else {
+    sortArray(updateRecipe, cacheUpdate, valuElmt, text);
+    eval(suggestId).innerHTML = "";
+    suggestList(updateRecipe);
+  }
+  eval(suggestId).innerHTML = ""
+  render();
+}
+
+
+function sortArray(array, cacheUpdate, valuElmt, text){
+  array.forEach( elmt => {
+    for ( let [key, value] of Object.entries(elmt)){
+      // console.log(key, valuElmt);
+      if( key === valuElmt && typeof value === 'object'){
+        for ( let item of value){
+          if( typeof item === 'object' && item.ingredient.toLowerCase() === text.toLowerCase()){
+            tagsIngredients.forEach( igrd => {
+              if( item.ingredient.toLowerCase() === igrd.toLowerCase()){
+                articleRecipes.innerHTML = "";
+                updateRecipe.push(elmt);
+                cacheUpdate.push(elmt);
+              }
+            })
+          } else if (typeof item === 'string' && item.toLowerCase() === text.toLowerCase()){
+            tagsUstensils.forEach( ustl => {
+              if( item.toLowerCase() === ustl.toLowerCase()){
+                console.log(elmt)
+                articleRecipes.innerHTML = "";
+                updateRecipe.push(elmt);
+                cacheUpdate.push(elmt);
+                
+              }
+                })
+              }
+            }
+          } else if ( key === valuElmt && typeof value === 'string' && value.toLowerCase() === text.toLowerCase() ){
+            tagsAplliance.forEach( apl => {
+              if( apl.toLowerCase() === apl.toLowerCase()){
+                console.log(elmt);
+                articleRecipes.innerHTML = "";
+            updateRecipe.push(elmt);
+            cacheUpdate.push(elmt);
+              }
+            })
+          }
+        }
+      })
+      console.log(updateRecipe);
+      cacheUpdate = [...new Set(cacheUpdate)];
+      updateRecipe = [...cacheUpdate];
+      console.log(updateRecipe);
+}
+
+const buttonSearch = document.getElementsByTagName('button')[0];
+buttonSearch.addEventListener('click', e => {
+  e.preventDefault();
+  searchMain();
+})
+
+function searchMain() {
+  console.log(inputSearch[0].value)
 }
